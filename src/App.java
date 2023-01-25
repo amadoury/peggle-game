56424693d83371ccddd599ef42172b5663a93eb7
Elias Abounaim <abounaim.elias@gmail.com>
2023-01-25 22:17:17 +0100

    nothing

--- src/App.java
+++ src/App.java
@@ -8,8 +8,6 @@ public class App extends JFrame {
 
     private JPanel left = new JPanel();
     private JPanel right = new JPanel();
-    private JPanel top = new JPanel();
-    private JPanel bottom = new JPanel();
 
     public App() {
         initUI();
@@ -21,36 +19,17 @@ public class App extends JFrame {
         setLayout(new GridBagLayout());
 
         GridBagConstraints c = new GridBagConstraints();
-        // c.fill = GridBagConstraints.HORIZONTAL;
-        // c.weightx = 0.5;
-        // c.weighty = 0.5;
-        // c.gridx = 0;
-        // c.gridy = 0;
-        // // c.gridwidth = 1;
-        // // c.gridheight = 1;
-        // // c.ipadx = 1;
-        // // c.ipady = 1;
-        // add(left, c);
-        // c.fill = GridBagConstraints.HORIZONTAL;
-        // c.gridx = 2;
-        // c.gridy = 0;
-        // add(right, c);
-        // // add(top, BorderLayout.NORTH);
-        // // add(bottom, BorderLayout.SOUTH);
-        // c.fill = GridBagConstraints.HORIZONTAL;
-        // c.gridx = 1;
-        // c.gridy = 0;
-        // add(new Board(dimensionFrame), c);
-        JButton button = new JButton("Button 1");
+
         c.fill = GridBagConstraints.HORIZONTAL;
+
         c.weightx = 0.5;
         c.gridx = 0;
         c.gridy = 0;
         add(left, c);
 
-        button = new JButton("Button 2");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.weightx = 3;
+        c.ipady = 1000;
 
         // System.out.println(dimensionFrame.getHeight());
         // System.out.println((int) dimensionFrame.getHeight());
@@ -58,13 +37,14 @@ public class App extends JFrame {
         c.gridy = 0;
         add(new Board(), c);
 
-        button = new JButton("Button 3");
         c.fill = GridBagConstraints.HORIZONTAL;
         c.weightx = 0.5;
         c.gridx = 2;
         c.gridy = 0;
         add(right, c);
 
+        pack();
+        dimensionFrame = this.getBounds().getSize();
         System.out.println(dimensionFrame);
 
         setTitle("App");
--- src/Board.java
+++ src/Board.java
@@ -5,6 +5,9 @@ import java.awt.*;
 public class Board extends JPanel {
 
     private Image imageBoard;
+    private JPanel top = new JPanel();
+    private JPanel bottom = new JPanel();
+    private JPanel center = new JPanel();
 
     public Board() {
         initBoard();
@@ -12,9 +15,39 @@ public class Board extends JPanel {
 
     private void initBoard() {
         loadImage("src/ressources/bgd-peggle-img-1.jpg");
+
         int width = imageBoard.getWidth(this);
         int height = imageBoard.getHeight(this);
         setPreferredSize(new Dimension(width, height));
+
+        setLayout(new GridBagLayout());
+
+        // GridBagConstraints c = new GridBagConstraints();
+
+        // c.fill = GridBagConstraints.VERTICAL;
+
+        // c.weightx = 0.5;
+        // c.gridx = 0;
+        // c.gridy = 0;
+        // c.ipadx = width;
+        // add(top, c);
+
+        // c.fill = GridBagConstraints.VERTICAL;
+        // c.weightx = 3;
+        // c.ipady = width;
+
+        // c.gridx = 0;
+        // c.gridy = 1;
+        // c.ipadx = width;
+        // add(center, c);
+
+        // c.fill = GridBagConstraints.VERTICAL;
+        // c.weightx = 0.5;
+        // c.gridx = 0;
+        // c.gridy = 2;
+        // c.ipadx = width;
+        // add(bottom, c);
+
     }
 
     private void loadImage(String path) {
