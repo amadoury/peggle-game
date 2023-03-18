1683b6e346da79d1eeb398e3ab0a1eced28ffd90
GUILLOUX aurelien <aurelien.guilloux@etu.u-paris.fr>
2023-03-18 11:59:41 +0100

    Update BoardEdit.java

--- src/BoardEdit.java
+++ src/BoardEdit.java
@@ -67,8 +67,22 @@ public class BoardEdit extends Board {
         g2d = (Graphics2D) g;
 
         g2d.drawImage(imageBoard, 0, 0, (int) width, (int) height, null);
-        if(editor.getListPeg().size() >= 1)
-            System.out.println("peg in editor : x = " + editor.getListPeg().get(0).getPegX() + "y = " + editor.getListPeg().get(0).getPegY());
+        if(editor.getListPeg().size() >= 1){
+            //System.out.println("peg in editor : x = " + editor.getListPeg().get(0).getPegX() + "y = " + editor.getListPeg().get(0).getPegY());
+            
+            try {
+                FileWriter writer = new FileWriter("level4.txt");
+                for(Peg e : editor.getListPeg()) {
+                    writer.write(e.pegX + "/" + e.pegY+"|" +e.getColor()+"\n");
+                }  
+                writer.close();
+            } catch(IOException e){
+                System.out.println("Une erreur est survenue lors de la création du fichier.");
+                e.printStackTrace();
+            } 
+
+            //afficher("level1.txt");
+        } 
     }
 
 
@@ -93,6 +107,34 @@ public class BoardEdit extends Board {
 
     public void setApp(App app){
         this.app = app ;
+    }
+    
+    // Fonction permettant de lire les coordonnées des pegs.
+    public static void afficher(String cheminFichier) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(cheminFichier));
+            String ligne;
+            while ((ligne = br.readLine()) != null) {
+                Pattern pattern = Pattern.compile("^(\\d+)/([\\d]+)\\|(.+)$");
+                Matcher matcher = pattern.matcher(ligne);
+                if (matcher.find()) {
+                    int x = Integer.parseInt(matcher.group(1));
+                    int y = Integer.parseInt(matcher.group(2));
+                    String c = matcher.group(3);
+                    //System.out.println("a: " + a + ", b: " + b + ", c: " + c);
+
+                }
+                /*
+                 *  Le premier groupe ^(\\d+) extrait la première série de chiffres avant le caractère / et le capture dans un groupe.
+                    Le deuxième groupe ([\\d]+)\\| extrait la deuxième série de chiffres après le caractère / et avant le caractère |, et le capture dans un groupe.
+                    Le troisième groupe (.+)$ extrait le reste de la ligne après le caractère | et le capture dans un groupe.
+                 */
+
+            }
+            br.close();
+        } catch (IOException e) {
+            System.err.println("Erreur lors de la lecture du fichier " + cheminFichier + " : " + e.getMessage());
+        }
     }
 
 }
