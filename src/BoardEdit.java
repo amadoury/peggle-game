4c9ca466ac54c44e908a847bb6866151e44a5eeb
GUILLOUX aurelien <aurelien.guilloux@etu.u-paris.fr>
2023-04-11 12:13:13 +0200

    Replace BoardEdit.java

--- src/BoardEdit.java
+++ src/BoardEdit.java
@@ -1,140 +1,173 @@
-import javax.swing.* ;
-import java.awt.* ;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseEvent;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.List ;
+import java.util.List;
 
 public class BoardEdit extends Board {
-    private PopUp popUp ;
-    private Editor editor ;
-    private int xMouse ; 
-    private int yMouse ;
-    private App app ;
+    private PopUp popUp;
+    private Editor editor;
+    private int xMouse;
+    private int yMouse;
+    private JButton valid_edit = new JButton("VALIDER");
+    private App app;
 
-    public BoardEdit() {
-        editor = new Editor() ;
+    public BoardEdit() throws FontFormatException, IOException {
+        super(1);
+        editor = new Editor();
 
-        List<String> listPath = new ArrayList<String>() ;
+        List<String> listPath = new ArrayList<String>();
         listPath.add("ressources/peg-orange.png");
         listPath.add("ressources/peg-bleu.png");
+        listPath.add("ressources/peg-bleu-rectangle.png");
+        listPath.add("ressources/peg-soleil.png");
+       // listPath.add("ressources/peg-bleu-rectangle.png") ;
 
-        popUp = new PopUp(listPath) ;
 
-        add(popUp) ;
 
-        for(int i = 0; i < popUp.getListMenuItem().size(); i++){
-            if(listPath.get(i).endsWith("orange.png")){
+        String path_font = "ressources/font_style/font.ttf";
+        InputStream is = MenuPrincipal.class.getResourceAsStream(path_font);
+        Font font = Font.createFont(Font.TRUETYPE_FONT, is).deriveFont(25f);
+        valid_edit.setFont(font);
+        valid_edit.setBounds((int) width/2-300,0,150,40);
+        add(valid_edit);
+
+        popUp = new PopUp(listPath);
+
+        add(popUp);
+
+        for (int i = 0; i < popUp.getListMenuItem().size(); i++) {
+            if (listPath.get(i).endsWith("orange.png")) {
                 popUp.getListMenuItem().get(i).addActionListener((event) -> {
                     /* pegCreation creation */
-                    PegCercle pegCercle = new PegCercle(xMouse, yMouse, 25, "orange") ;
+                    PegCercle pegCercle = new PegCercle(xMouse,yMouse, (int) (25 * (resolutionScreen / 100)),
+                            "orange");
 
-                    /* add peg to editor */ 
-                    editor.addPeg(pegCercle);
-
-                    /* add to boardEdit */                   
-                    add(pegCercle.getJlabel()) ;
-
-                    /* set : isEditing, App, BoardEdit */
-                    pegCercle.getJlabel().setEditing(true);
-                    pegCercle.getJlabel().setApp(app); 
-                    pegCercle.getJlabel().setBoardEdit(this);
-                    pegCercle.getJlabel().setPeg(pegCercle) ;
-                    repaint();                   
-                });
-            }
-            else{
-                popUp.getListMenuItem().get(i).addActionListener((event) -> {
-                    PegCercle pegCercle = new PegCercle(xMouse, yMouse, 25, "bleu") ;
-
-                    /* add peg to editor */ 
+                    /* add peg to editor */
                     editor.addPeg(pegCercle);
 
                     /* add to boardEdit */
-                    add(pegCercle.getJlabel()) ;
+                    add(pegCercle.getLabelPeg());
+
+                    /* set : isEditing, App, BoardEdit */
+                    pegCercle.getLabelPeg().setEditing(true);
+                    pegCercle.getLabelPeg().setApp(app);
+                    pegCercle.getLabelPeg().setBoardEdit(this);
+                    pegCercle.getLabelPeg().setPeg(pegCercle);
+                    repaint();
+                });
+            } else if (listPath.get(i).endsWith("bleu.png")) {
+                popUp.getListMenuItem().get(i).addActionListener((event) -> {
+                    PegCercle pegCercle = new PegCercle(xMouse,yMouse, (int) (25 * (resolutionScreen / 100)), "bleu");
+
+                    /* add peg to editor */
+                    editor.addPeg(pegCercle);
+
+                    /* add to boardEdit */
+                    add(pegCercle.getLabelPeg());
 
                     /* set : isEditing, App, BoardEdit, Peg */
-                    pegCercle.getJlabel().setEditing(true);
-                    pegCercle.getJlabel().setApp(app);
-                    pegCercle.getJlabel().setBoardEdit(this);
-                    pegCercle.getJlabel().setPeg(pegCercle) ;
+                    pegCercle.getLabelPeg().setEditing(true);
+                    pegCercle.getLabelPeg().setApp(app);
+                    pegCercle.getLabelPeg().setBoardEdit(this);
+                    pegCercle.getLabelPeg().setPeg(pegCercle);
+                    repaint();
+                });
+            } else if (listPath.get(i).endsWith("bleu-rectangle.png")) {
+                popUp.getListMenuItem().get(i).addActionListener((event) -> {
+                    PegRectangle pegRectangle = new PegRectangle(xMouse,yMouse, (int) (50 * (resolutionScreen / 100)),
+                            (int) (25 * (resolutionScreen / 100)), "bleu");
+
+                    /* add peg to editor */
+                    editor.addPeg(pegRectangle);
+
+                    /* add to boardEdit */
+                    add(pegRectangle.getLabelPeg());
+
+                    /* set : isEditing, App, BoardEdit, Peg */
+                    pegRectangle.getLabelPeg().setEditing(true);
+                    pegRectangle.getLabelPeg().setApp(app);
+                    pegRectangle.getLabelPeg().setBoardEdit(this);
+                    pegRectangle.getLabelPeg().setPeg(pegRectangle);
+                    repaint();
+                });
+            } else if (listPath.get(i).endsWith("soleil.png")){
+                popUp.getListMenuItem().get(i).addActionListener((event) -> {
+                    PegSoleil pegSoleil = new PegSoleil(xMouse,yMouse,(int) (50 * (resolutionScreen / 100)), "soleil");
+                    editor.addPeg(pegSoleil);
+                    add(pegSoleil.getLabelPeg());
+
+                    pegSoleil.getLabelPeg().setEditing(true);
+                    pegSoleil.getLabelPeg().setApp(app);
+                    pegSoleil.getLabelPeg().setBoardEdit(this);
+                    pegSoleil.getLabelPeg().setPeg(pegSoleil);
                     repaint();
                 });
             }
         }
+
+
+
+        valid_edit.addActionListener((event) -> {
+            WriteLevelText();
+        });
     }
 
     public void paintComponent(Graphics g){
         g2d = (Graphics2D) g;
+        g2d.drawImage(imageBoard, 0, 0, (int) width, (int) height, null); 
+    }
 
-        g2d.drawImage(imageBoard, 0, 0, (int) width, (int) height, null);
+    // public void setWidthScreen(double w) {
+    // double var = w - (2.0 / 8.0) * w;
+    // width = var;
+    // }
+
+    // public void setHeightScreen(double w) {
+    // height = w;
+    // }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON3) {
+            popUp.show(this, e.getX(), e.getY());
+            xMouse = e.getX();
+            yMouse = e.getY();
+        }
+    }
+
+    public void setApp(App app) {
+        this.app = app;
+    }
+
+
+    public void WriteLevelText(){
+        // System.out.println("OK");
         if(editor.getListPeg().size() >= 1){
-            //System.out.println("peg in editor : x = " + editor.getListPeg().get(0).getPegX() + "y = " + editor.getListPeg().get(0).getPegY());
-            
             try {
-                FileWriter writer = new FileWriter("level4.txt");
+                FileWriter writer = new FileWriter("level3.txt");
                 for(Peg e : editor.getListPeg()) {
-                    writer.write(e.pegX + "/" + e.pegY+"|" +e.getColor()+"\n");
+                    String type;
+                    if(e instanceof PegCercle) type = "PegCercle";
+                    else if(e instanceof PegRectangle) type = "PegRectangle";
+                    else type = "PegSoleil";
+                    writer.write((e.pegX/width) + "/" + (e.pegY/height)+"/" + e.getColor() + "/" +type + "\n");
                 }  
                 writer.close();
             } catch(IOException e){
                 System.out.println("Une erreur est survenue lors de la création du fichier.");
                 e.printStackTrace();
             } 
-
-            //afficher("level1.txt");
         } 
     }
 
-
-    // public void setWidthScreen(double w) {
-    //     double var = w - (2.0 / 8.0) * w;
-    //     width = var;
-    // }
-
-    // public void setHeightScreen(double w) {
-    //     height = w;
-    // }
-
-
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        if (e.getButton() == MouseEvent.BUTTON3) {
-            popUp.show(this, e.getX() , e.getY());
-            xMouse = e.getX() ;
-            yMouse = e.getY() ;
-        }
+    public Editor getEditor() {
+        return editor;
     }
 
-    public void setApp(App app){
-        this.app = app ;
-    }
-    
-    // Fonction permettant de lire les coordonnées des pegs.
-    public static void afficher(String cheminFichier) {
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(cheminFichier));
-            String ligne;
-            while ((ligne = br.readLine()) != null) {
-                Pattern pattern = Pattern.compile("^(\\d+)/([\\d]+)\\|(.+)$");
-                Matcher matcher = pattern.matcher(ligne);
-                if (matcher.find()) {
-                    int x = Integer.parseInt(matcher.group(1));
-                    int y = Integer.parseInt(matcher.group(2));
-                    String c = matcher.group(3);
-                    //System.out.println("a: " + a + ", b: " + b + ", c: " + c);
-
-                }
-                /*
-                 *  Le premier groupe ^(\\d+) extrait la première série de chiffres avant le caractère / et le capture dans un groupe.
-                    Le deuxième groupe ([\\d]+)\\| extrait la deuxième série de chiffres après le caractère / et avant le caractère |, et le capture dans un groupe.
-                    Le troisième groupe (.+)$ extrait le reste de la ligne après le caractère | et le capture dans un groupe.
-                 */
-
-            }
-            br.close();
-        } catch (IOException e) {
-            System.err.println("Erreur lors de la lecture du fichier " + cheminFichier + " : " + e.getMessage());
-        }
-    }
 
 }
