54c8663dc50bdb320b11a3a53e57ed11a4325b3c
GUILLOUX aurelien <aurelien.guilloux@etu.u-paris.fr>
2023-05-01 16:50:43 +0200

    modifications MenuPrincipal.java

--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -1,101 +1,177 @@
-/*
- * @author Aurelien 
- */
-import java.awt.Font;
-import java.awt.FontFormatException;
-import java.io.IOException;
-import java.io.InputStream;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.ImageIcon;
+import javax.swing.border.Border;
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontFormatException;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.swing.ImageIcon;
+import java.awt.event.*;
+
 
 
 
 public class MenuPrincipal extends JFrame {
-    private ImagePanel PanelImage = new ImagePanel(new ImageIcon("src/ressources/menu.png").getImage());
+    private ImagePanel PanelImage = new ImagePanel(new ImageIcon("src/ressources/menu.jpg").getImage());
     private JPanel ButtonPanel = new JPanel();
-    private JButton playButton = new JButton("PLAY");
-    private JButton paramButton = new JButton("SETTINGS");
-    private JButton exitButton = new JButton("EXIT");
+
+    private int width,height;
+    private JButton playButton = createButton("PLAY",(int)(width*0.64),(int)(height*0.35));
+    private JButton editButton = createButton("EDITOR",(int)(width*0.64),(int)(height*0.5));
+    private JButton exitButton = createButton("EXIT",(int)(width*0.64),(int)(height*0.65));
 
     public MenuPrincipal() throws FontFormatException, IOException {
-      super();
+      Dimension screensize = Toolkit.getDefaultToolkit().getScreenSize();
       setTitle("JEU");
       setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      setSize(1400, 1000);
+      setSize(1920, 1080);
       setResizable(false);
-
-      String path_font = "ressources/font_style/font.ttf";
-      InputStream is = MenuPrincipal.class.getResourceAsStream(path_font);
-      Font font = Font.createFont(Font.TRUETYPE_FONT,is).deriveFont(65f);
-
-      Color c = new Color(207, 159, 255);
-      playButton.setFont(font); playButton.setBackground(c);
-      paramButton.setFont(font); paramButton.setBackground(c);
-      exitButton.setFont(font); exitButton.setBackground(c);
+      width = (int)screensize.getWidth();
+      height = (int)screensize.getHeight();
 
       ButtonPanel.setLayout(null);
-      playButton.setBounds(475,700,215,105);
-      paramButton.setBounds(715,700,440,105);
-      exitButton.setBounds(1180,700,198,105);
+      ButtonPanel.setOpaque(false);
+      ButtonPanel.setPreferredSize(new Dimension(1920, 1080));
+
+      playButton.setBounds((int)(width*0.64),(int)(height*0.35),325,105);
+      editButton.setBounds((int)(width*0.64),(int)(height*0.5),325,105);
+      exitButton.setBounds((int)(width*0.64),(int)(height*0.65),325,105);
 
 
 
-      ButtonPanel.add(playButton); ButtonPanel.add(paramButton); ButtonPanel.add(exitButton);
+      ButtonPanel.add(playButton); ButtonPanel.add(editButton); ButtonPanel.add(exitButton);
         
         playButton.addActionListener(e -> 
         {
-            // renvoie vers le menuNiveau
-            try {
-                PlayActionPerformed(e);
-            } catch (FontFormatException e1) {
-                e1.printStackTrace();
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            }
+          // renvoie vers le menuNiveau
+          PlayActionPerformed(e);
         });
 
-        paramButton.addActionListener(e -> 
+        editButton.addActionListener(e -> 
         {
-            // renvoie vers le menu des paramètres
-            ParamActionPerformed(e);
+          // renvoie vers l'éditeur
+          EditorActionPerformed(e);
         });
 
         exitButton.addActionListener(e ->
         {
           ExitActionPerformed(e);
         });
-      
+
         this.getContentPane().add(PanelImage);
         this.getContentPane().add(ButtonPanel);
         this.setVisible(true);
     }
 
-    private void PlayActionPerformed(java.awt.event.ActionEvent evt) throws FontFormatException, IOException {                                         
+    private void PlayActionPerformed(java.awt.event.ActionEvent evt) {                                         
       // Level Selection menu
-      /*
-      this.dispose();
-      LoadingScreen load = new LoadingScreen();
-      //load.setVisible(true);
-      */
     }                                        
 
-    private void ParamActionPerformed(java.awt.event.ActionEvent evt) {                                         
-        // Parameters Menu
+    private void EditorActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        // Editor
     }                                        
 
     private void ExitActionPerformed(java.awt.event.ActionEvent evt) {                                         
         System.exit(0);
     }  
 
-    /*
-    public static void main(String[] args) throws FontFormatException, IOException{
-      new MenuPrincipal();
+    private JButton createButton(String text,int width,int height) throws FontFormatException, IOException{
+      JButton button = new JButton(text);
+
+      String path_font = "ressources/font_style/font.ttf";
+      InputStream is = LevelMenu.class.getResourceAsStream(path_font);
+      Font font = Font.createFont(Font.TRUETYPE_FONT, is).deriveFont(65f);
+      button.setFocusPainted(false);
+      button.setFont(font);
+      Color colorPrinciaple = new Color(212,226,8);
+      Color colorClicked = new Color(165,176,3);
+      Color textColor = new Color(153,0,153);
+      button.setBackground(colorPrinciaple);
+      button.setForeground(textColor);
+
+
+      button.addMouseListener(new MouseAdapter() {
+        public void mouseEntered(MouseEvent e) {
+            button.setBackground(Color.WHITE);
+            button.setForeground(Color.BLACK);
+        }
+      });
+
+      button.addMouseListener(new MouseAdapter() {
+        public void mouseExited(MouseEvent e) {
+            button.setBackground(colorPrinciaple);
+            button.setForeground(textColor);
+        }
+      });
+
+      button.addActionListener(new ActionListener() {
+        public void actionPerformed(ActionEvent e){
+          button.setBackground(colorClicked);
+        }
+      });
+
+      return button;
     }
-    */
-    
+
+  class ImagePanel extends JPanel {
+
+    private Image img;
+  
+    public ImagePanel(String img) {
+      this(new ImageIcon(img).getImage());
+    }
+  
+    public ImagePanel(Image img) {
+      this.img = img;
+      Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+      setPreferredSize(size);
+      setMinimumSize(size);
+      setMaximumSize(size);
+      setSize(1920,1080);
+      setLayout(null);
+    }
+  
+    public void paintComponent(Graphics g) {
+      g.drawImage(img, 0, 0, null);
+    }
 }
+
+
+    // class RoundBtn implements Border {
+    //   private int r;
+    //   RoundBtn(int r) {
+    //       this.r = r;
+    //   }
+    //   public Insets getBorderInsets(Component c) {
+    //       return new Insets(this.r+1, this.r+1, this.r+2, this.r);
+    //   }
+    //   public boolean isBorderOpaque() {
+    //       return true;
+    //   }
+    //   public void paintBorder(Component c, Graphics g, int x, int y, 
+    //   int width, int height) {
+    //       g.drawRoundRect(x, y, width-1, height-1, r, r);
+    //   }
+    // }  
+
+  public static void main(String[] args) throws FontFormatException, IOException{
+    new MenuPrincipal();
+  }
+}
+
+
+
 
 
